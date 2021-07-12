bbead8f81a612adabb4c889d1b6aea2a63ce8922
Jhen <79475798+jhen0605@users.noreply.github.com>
2021-07-12 23:14:25 +0800

    1

--- photo2/ViewController.swift
+++ photo2/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import CoreImage.CIFilterBuiltins //內建濾鏡
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func filterSliderChange(_ sender: UISlider) {
+        let image = UIImage(named: "馬路")                       //產生CIImage
+              let ciImage = CIImage(image: image!)              //產生CIImage
+              let filter = CIFilter.colorMonochrome()       //製造濾鏡效果
+              filter.inputImage = ciImage                       //設定 CIFilter
+              filter.intensity = sender.value                   //設定 CIFilter
+              if let outputCIImage = filter.outputImage {   //產生加了濾鏡效果
+                  let filterImage = UIImage(ciImage: outputCIImage)
+                  imageView.image = filterImage
+              }
+    }
 }
-
